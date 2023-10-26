48d18952aaa3b9cf2ec584cf2a787996880336df
Aytuğ <umut-aytug@hotmail.com>
2023-10-26 09:28:12 +0300

    Template of the project

--- Group21.java
+++ Group21.java
@@ -1,10 +1,49 @@
 import java.util.Scanner;
 public class Group21 {
-    public static void menu(String[] args) {
+    public static void menu(){
+
+    }
+    public static void addition(){
+
+    }
+    public static void subtraction(){
+
+    }
+    public static void multiplication(){
+
+    }
+    public static void division(){
+
+    }
+    public static void scalarMultiplication(){
+
+    }
+    public static void transpose(){
+
+    }
+    public static void inverse(){
+
+    }
+    public static void trace(){
+
+    }
+    public static void adjoint(){
+
+    }
+    public static void orthogonal(){
+
+    }
+    public static void clearTerminal(){
 
     }
     public static void main(String[] args) {
 
     }
-    
+    // _____________________
+    // |Group 21           |
+    // |Başar Çelebi       |
+    // |Emircan Çapkan     |
+    // |Göktuğ Ateş        |
+    // |Umut Aytuğ Semerci |
+    // |___________________|
 }
