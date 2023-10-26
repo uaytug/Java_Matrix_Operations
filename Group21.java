df9d30a6fbd56ed61d6213e5e85fe349a328fbe1
Başar Çelebi <basarcelebi02@gmail.com>
2023-10-26 15:20:09 +0300

    Orthogonal function was created. TODOs was added.

--- Group21.java
+++ Group21.java
@@ -30,7 +30,68 @@ public class Group21 {
     public static void adjoint(){
 
     }
-    public static void orthogonal(){
+    /* An orthogonal matrix is a specially defined square matrix such as, 2x2, 3x3, 4x4 etc. . */
+    public static boolean orthogonal(int [][]matrix, int rowNumber, int columnNumber){
+        // firstly, we should check row and column numbers. If there is no equality, this means the matrix is not orthogonal.
+        if (rowNumber != columnNumber)
+        {
+            return false;
+        }
+
+        
+		
+		// Secondly, we should create nested for loop to find the transpose of the matrix.
+		int [][]transpose = new int[columnNumber][rowNumber];
+		for (int i = 0; i < columnNumber; i++)
+        {
+            for (int j = 0; j < rowNumber; j++)
+            {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+
+        /* -- TODO -- : We should compare the equality of the inverse matrix and transpose matrix */
+			
+		
+		/* To find the product of the input matrix and its transpose, we coded this 3 for loops. */
+        // We declared new matrix that is called product in order to store product results.
+		int [][]product = new int[rowNumber][columnNumber];
+        
+		for (int i = 0; i < rowNumber; i++) 
+		{
+            
+			for (int j = 0; j < columnNumber; j++) 
+			{
+		
+				int sum = 0; // we declared a variable for the sum of the products. In every iterations, it will start from zero.
+				for (int k = 0; k < columnNumber; k++) 
+				{
+					// To multiply the input matrix and its transpose, we created this operation.
+					sum += (matrix[i][k] * transpose[k][j]);
+				}
+			
+				product[i][j] = sum;
+			}
+		}
+		
+		/* Now we should check our product matrix. If it is identity matrix, our main matrix must be orthogonal. If not, it cannot be an orthogonal matrix. To check the product matrix, we should create a nested for loop again. */ 
+		for (int i = 0; i < rowNumber; i++) 
+		{
+			for (int j = 0; j < columnNumber; j++) 
+			{
+				if (i != j && product[i][j] != 0)
+                {
+                    return false;
+                }else if (i == j && product[i][j] != 1)
+                {
+                    return false;
+                }	
+			}
+		}
+		
+        // If there is no problem until this code line, our input matrix must be an orthogonal matrix.
+		return true;
 
     }
     public static void clearTerminal(){
